b8843c76fbe86aeb73d01cd99e29f8ff22510a55
shervin karimi <sherv_sh@hotmail.com>
2021-03-05 15:36:29 +0100

    e

--- app.py
+++ app.py
@@ -4,54 +4,66 @@ import requests
 import numpy as np
 import pandas as pd
 
+st.image('image.jpg')
+
+
+st.markdown("<h1 style='text-align: center; color: dark-red;'>FIRE TAXI</h1>", unsafe_allow_html=True)
+
 '''
-# TaxiFareModel front
+# When
+'''
+st.date_input('')
+st.time_input('')
+
+
+'''
+# Where
 '''
 
-st.markdown('''
-Enter your coordinnates
-''')
 
 
 
 # PU lon ----------------------------------------------------------------
-pickup_longitude = st.number_input('Insert a pickup_longitude')
+pickup_longitude = st.number_input('Insert a pickup_longitude',value=-73)
 
-st.write('The current pickup_longitude is ', pickup_longitude)
+# st.write('The current pickup_longitude is ', pickup_longitude)
 # PU lat ----------------------------------------------------------------
-pickup_latitude = st.number_input('Insert a pickup_latitude')
+pickup_latitude = st.number_input('Insert a pickup_latitude',value=43)
 
-st.write('The current pickup_latitude is ', pickup_latitude)
+# st.write('The current pickup_latitude is ', pickup_latitude)
 # DO lat ----------------------------------------------------------------
-dropoff_longitude = st.number_input('Insert a dropoff_longitude')
+dropoff_longitude = st.number_input('Insert a dropoff_longitude',value=-74)
 
-st.write('The current dropoff_longitude is ', dropoff_longitude)
+# st.write('The current dropoff_longitude is ', dropoff_longitude)
 # DO lat ----------------------------------------------------------------
-dropoff_latitude = st.number_input('Insert a dropoff_latitude')
+dropoff_latitude = st.number_input('Insert a dropoff_latitude',value=42)
 
-st.write('The current dropoff_latitude is ', dropoff_latitude)
+# st.write('The current dropoff_latitude is ', dropoff_latitude)
 
 
-st.markdown('''
 
-Maps
 
-''')
 df = pd.DataFrame(
     {
         'lat':[pickup_latitude,dropoff_latitude],
         'lon':[pickup_longitude,dropoff_longitude]
     }
 )
+
+
+'''
+# Who
+'''
+
+st.radio('', [1,2,3,4])
+
+'''
+# Maps
+'''
 st.map(df)
-
-
 
 url = 'http://taxifare.lewagon.ai/predict_fare/'
 
-if url == 'http://taxifare.lewagon.ai/predict_fare/':
-
-    st.markdown('Taxi Fare Estimation')
 
 
 input_data= {
@@ -65,4 +77,18 @@ input_data= {
 
 #url ='http://taxifare.lewagon.ai/predict_fare/?key=2012-10-06%2012:10:20.0000001&pickup_datetime=2012-10-06%2012:10:20%20UTC&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2'
 response = requests.get(url,params=input_data)
-st.write(response.json()['prediction'])
+price = round(response.json()['prediction'],0)
+
+
+
+'''
+# Let's go
+'''
+st.subheader('Estimated Price')
+st.code('$'+str(price))
+
+st.button('Order your Taxi Fire')
+
+
+
+
